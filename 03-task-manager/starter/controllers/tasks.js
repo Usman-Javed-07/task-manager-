ae4da20caf8d178c9affabc6f576e754053cdf9a
Usman Javed <usmanjaved0816@gmail.com>
2024-11-07 20:44:37 +0500

    completd get task section

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,17 +1,77 @@
-const getAllTasks = (req , res) => {
-    res.send('get all tasks')
+const Task = require('../models/task')
+
+const getAllTasks = async (req , res) => {
+   try {
+    const task = await Task.find({})
+    res.status(200).json( {task })
+   } catch (error) {
+    res.status(500).json({msg: error})
+   }
 }
 
 
-const createTask = (req , res) => {
-    res.json(req.body)
+const createTask = async (req , res) => {
+    
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json( {task })
+        
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
 }
 
 
-const getTask = (req , res) => {
-    res.json({id:req.params.id})
+
+// 
+
+
+
+const getTask = async (req , res) => {
+
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOne({  _id:taskID})
+        if(!task) {
+            return res.status(404).json({msg:`No task with id ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
 }
 
+
+
+// 
+// const mongoose = require('mongoose');
+
+// const getTask = async (req, res) => {
+//     try {
+//         const { id: taskID } = req.params;
+
+//         // Check if the provided ID is a valid ObjectId
+//         if (!mongoose.Types.ObjectId.isValid(taskID)) {
+//             return res.status(404).json({ msg: `Invalid ID format: ${taskID}` });
+//         }
+
+//         // Use `findById` to find the document by its _id
+//         const task = await Task.findById(taskID);
+//         if (!task) {
+//             return res.status(400).json({ msg: `No task found with ID ${taskID}` });
+//         }
+
+//         res.status(200).json({ task })
+//     } catch (error) {
+//         res.status(500).json({ msg: error.message });
+//     }
+// };
+
+
+
+
+
+// 
 
 const updateTask = (req , res) => {
     res.send('update task')
