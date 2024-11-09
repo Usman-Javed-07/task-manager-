e975f0ffd84db98c8009cd5702768ef77862fecf
Usman Javed <usmanjaved0816@gmail.com>
2024-11-09 23:11:22 +0500

    connecting to database

--- 04-store-api/starter/populate.js
+++ 04-store-api/starter/populate.js
@@ -1,11 +1,13 @@
 const connectDB = require('./db/connect')
-const product = require('./models/product')
+const Product = require('./models/product')
 
 const jsonProduct = require('./product.json')
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
+        await Product.deleteMany()
+        await Product.create(jsonProduct)
         console.log('success !!!');
         
     } catch (error) {
