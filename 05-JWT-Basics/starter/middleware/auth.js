3365c95c985b0be378ce0a3f965b0b4293f80829
Usman Javed <usmanjaved0816@gmail.com>
2024-11-11 12:23:57 +0500

    start working on JWT

---
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -0,0 +1,9 @@
+// // auth.js
+// const express = require('express');
+// const router = express.Router();
+
+// router.get('/', (req, res) => {
+//   res.send('Hello from auth route');
+// });
+
+// module.exports = router;
